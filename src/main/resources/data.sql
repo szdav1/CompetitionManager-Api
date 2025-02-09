84d8c070350f3a6a032dc8c00e753026695b7830
SzDave <szantodavid.mail@gmail.com>
2025-02-10 00:57:09 +0100

    Add more competitors to the in-memory database.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -19,6 +19,105 @@ values ('Szurke Levente', 'Pince CLUB', '2005-12-01');
 insert into competitor (name, club, birth_date)
 values ('Szanto David', 'Minek Vagy Itt CLUB', '2005-02-01');
 
+insert into competitor (name, club, birth_date)
+values ('Kis Miklos', 'Chips CLUB', '2015-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Tamas Gabor', 'Apatok itt van irany a Jafi', '2899-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Olah Armando', 'Kell a vas more', '2002-02-01');
+
+insert into competitor (name, club, birth_date)
+values ('Lakatos Boeing 627', 'Guszev', '1005-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Rezmives Manuella', 'Tuti nem cigany', '2012-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Doglott Hintalo', 'Hinta CLUB', '2005-02-01');
+
+insert into competitor (name, club, birth_date)
+values ('Szabo Miklos', 'Nyirszikszi CLUB', '2005-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Szurke Ronaldo', 'Pince CLUB', '2005-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Szanto Gergo', 'Minek Vagy Itt CLUB', '2005-02-01');
+
+insert into competitor (name, club, birth_date)
+values ('Kis Adnras', 'Chips CLUB', '2015-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Tamas Tamas', 'Apatok itt van irany a Jafi', '2899-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Olah Cigany', 'Kell a vas more', '2002-02-01');
+
+insert into competitor (name, club, birth_date)
+values ('Lakatos BMW 525', 'Guszev', '1005-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Rezmives Manuella apja', 'Tuti nem cigany', '2012-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Negecizz Velemella', 'Hinta CLUB', '2005-02-01');
+
+insert into competitor (name, club, birth_date)
+values ('Szabo David', 'Nyirszikszi CLUB', '2005-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Szurke Levente', 'Pince CLUB', '2005-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Szanto David', 'Minek Vagy Itt CLUB', '2005-02-01');
+
+insert into competitor (name, club, birth_date)
+values ('Kis Miklos', 'Chips CLUB', '2015-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Tamas Gabor', 'Apatok itt van irany a Jafi', '2899-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Olah Armando', 'Kell a vas more', '2002-02-01');
+
+insert into competitor (name, club, birth_date)
+values ('Lakatos Boeing 627', 'Guszev', '1005-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Rezmives Manuella', 'Tuti nem cigany', '2012-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Doglott Hintalo', 'Hinta CLUB', '2005-02-01');
+
+insert into competitor (name, club, birth_date)
+values ('Szabo Miklos', 'Nyirszikszi CLUB', '2005-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Szurke Ronaldo', 'Pince CLUB', '2005-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Szanto Gergo', 'Minek Vagy Itt CLUB', '2005-02-01');
+
+insert into competitor (name, club, birth_date)
+values ('Kis Adnras', 'Chips CLUB', '2015-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Tamas Tamas', 'Apatok itt van irany a Jafi', '2899-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Olah Cigany', 'Kell a vas more', '2002-02-01');
+
+insert into competitor (name, club, birth_date)
+values ('Lakatos BMW 525', 'Guszev', '1005-01-01');
+
+insert into competitor (name, club, birth_date)
+values ('Rezmives Manuella apja', 'Tuti nem cigany', '2012-12-01');
+
+insert into competitor (name, club, birth_date)
+values ('Negecizz Velemella', 'Hinta CLUB', '2005-02-01');
+
 
 -- LEADERBOARD
 insert into leaderboard (competition_id, competitor_id, placement)
